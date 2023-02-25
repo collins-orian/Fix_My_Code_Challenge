cb9d0d8749bf664b93774892c8d77dca16177092
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2023-02-26 00:16:21 +0100

    Update delete_dnodeint_at_index.c

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,8 +43,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		tmp = *head;
-		(*head)->next->prev = (*head)->prev;
+		(*head)->prev->next = (*head)->next;
 		free(*head);
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
